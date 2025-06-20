f85976d0caa2635705674ee073b0a29f883fa492
Developergatsby <gabokekeemmanuel@gmail.com>
2025-06-20 21:57:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { AllRoutes } from "./routes/AllRoutes"; 
+import { Header, Footer, MovieCard } from "./components";
+import "./App.css";
+
+function App() {
+  return (
+  <>
+  <Header />
+    <AllRoutes />
+  </>
+  )
+  
+}
+
+export default App;
---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MovieList = () => {
+  return( <main>
+    <div className="text-9xl">
+      No list
+    </div>
+    MovieList
+    
+    </main>);
+};
